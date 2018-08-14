1f82fbd1dbba7c4cfa8876ca10e3a3c998b2077e
yurina <yurimel33@gmail.com>
2018-08-15 00:20:32 +0900

    [Add]Baseプロジェクト

--- CountDownAlpha/AddCowntDownSetView.swift
+++ CountDownAlpha/AddCowntDownSetView.swift
@@ -7,16 +7,34 @@
 //
 
 import UIKit
+import TextFieldEffects
 
 class AddCountDownSetView : UIViewController {
     
-//    @IBOutlet weak var dataTo: UIPickerView!
-
+    //    @IBOutlet weak var dataTo: UIPickerView!
+    @IBOutlet weak var titleTextField: AkiraTextField!
+    @IBOutlet weak var endDateTextField: AkiraTextField!
     
+    @IBOutlet weak var titleErrorMsg: UILabel!
+    @IBOutlet weak var endDateErrorMsg: UILabel!
+    
+    @IBOutlet weak var sunday: UISwitch!
+    @IBOutlet weak var monday: UISwitch!
+    @IBOutlet weak var tuesday: UISwitch!
+    @IBOutlet weak var wednesday: UISwitch!
+    @IBOutlet weak var thursday: UISwitch!
+    @IBOutlet weak var friday: UISwitch!
+    @IBOutlet weak var saturday: UISwitch!
+    @IBOutlet weak var horiday: UISwitch!
+    
+    @IBOutlet weak var createBt: UIButton!
+    
+    var toolBar:UIToolbar!
+
     
     // 設定データ文字列（0 = カウントしない / 1 = カウントする）
     private var setNo : String = ""
-    private var setTirtle : String = ""
+    private var setTitle : String = ""
     private var setDateTo : String = ""
     private var setSunday : String = "1"
     private var setMonday : String = "1"
@@ -32,27 +50,163 @@ class AddCountDownSetView : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 終了日Picker上のtoolbarのdoneボタン
+        toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let toolBarBtn = UIBarButtonItem(title: "DONE", style: .plain, target: self, action: #selector(self.doneBtn))
+        toolBar.items = [toolBarBtn]
+        endDateTextField.inputAccessoryView = toolBar
         
-        if setSettingData(setData: createSetData()) {
-            // 設定データ書き込み成功
-            
-        } else {
-            // 設定データ書き込み失敗
-            
-        }
+        // 終了日の初期値 = 今日
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat  = "yyyy.MM.dd";
+        endDateTextField.text = dateFormatter.string(from: Date())
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    /**
+     終了日Pickerを表示
+     */
+    @IBAction func endDateFieldAction(_ sender: UITextField) {
+        // Picker生成
+        let datePickerView : UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        datePickerView.locale = Locale.init(identifier: "ja_JP")
+        datePickerView.minimumDate = Date()
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action:  #selector(self.datePickerValueChanged), for: UIControlEvents.valueChanged)
+    }
     
-    private func createSetData() -> String {
+    //datepickerが選択されたらtextfieldに表示
+    @objc func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat  = "yyyy.MM.dd";
+        endDateTextField.text = dateFormatter.string(from: sender.date)
+    }
+
+    @objc func doneBtn(){
+        endDateTextField.resignFirstResponder()
+    }
+    
+    @IBAction func createBtAction(_ sender: Any) {
+        let title = titleTextField.text
+        let endDate = endDateTextField.text
         
+        // テキスト記入チェック
+        if title == "" {
+            // 終了日が未記入だったら、警告を出す
+            titleErrorMsg.isHidden = false
+        } else {
+            titleErrorMsg.isHidden = true
+        }
+        
+        if endDate == "" || endDate?.count != 10 {
+            // 終了日が未記入だったら、警告を出す
+            endDateErrorMsg.isHidden = false
+        } else {
+            endDateErrorMsg.isHidden = true
+        }
+        
+        if title == "" || endDate == "" || endDate?.count != 10 {
+            // どれかが未記入だったら以降の処理を実行しない
+            return
+        }
+
+        setNo = "2"
+        setTitle(title: title!)
+        setDateTo = endDate!
+        setCountDay()
+
+        // 設定データ書き込み処理を開始する
+        startCreateData()
+    }
+    
+    /**
+     タイトルの文字列を作成する
+     */
+    private func setTitle(title: String) {
+        setTitle = title.replacingOccurrences(of:".", with:"")
+    }
+    
+    /**
+     カウントする日を設定する
+    */
+    private func setCountDay() {
+        if sunday.isOn {
+            setSunday = "1"
+        } else {
+            setSunday = "0"
+        }
+        if monday.isOn {
+            setMonday = "1"
+        } else {
+            setMonday = "0"
+        }
+        if tuesday.isOn {
+            setTuesday = "1"
+        } else {
+            setTuesday = "0"
+        }
+        if wednesday.isOn {
+            setWednesday = "1"
+        } else {
+            setWednesday = "0"
+        }
+        if thursday.isOn {
+            setThursday = "1"
+        } else {
+            setThursday = "0"
+        }
+        if friday.isOn {
+            setFriday = "1"
+        } else {
+            setFriday = "0"
+        }
+        if saturday.isOn {
+            setSaturday = "1"
+        } else {
+            setSaturday = "0"
+        }
+        if horiday.isOn {
+            setHoriday = "1"
+        } else {
+            setHoriday = "0"
+        }
+    }
+    
+    private func startCreateData() {
+
+        if setSettingData(setData: createSetData()) {
+            // 設定データ書き込み成功
+            print("成功")
+            
+        } else {
+            // 設定データ書き込み失敗
+            print("失敗")
+            let alertView = UIAlertController(title: "",message: "作成に失敗しました。", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default){ (action: UIAlertAction) in
+                //        ②-2 OKがクリックされた時の処理
+                
+                
+            }
+            alertView.addAction(okAction)
+            present(alertView,animated: true,completion: nil)
+
+        }
+    }
+    
+    /**
+     設定データを作成する
+     */
+    private func createSetData() -> String {
         
         var createData : String = ""
         createData.append(setNo + ",")
-        createData.append(setTirtle + ",")
+        createData.append(setTitle + ",")
         createData.append(setDateTo + ",")
         createData.append(setSunday + ",")
         createData.append(setMonday + ",")
---
+++ CountDownAlpha/CountViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CountViewCell.swift
+//  CountDownAlpha
+//
+//  Created by yurina on 2018/08/07.
+//  Copyright © 2018年 yurina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CountViewCell : UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var endDay: UILabel!
+    @IBOutlet weak var countDown: UILabel!
+    
+}
